ebd652bfbde60696084b6839ed7bb5176e0ac052
riazakhanda017ksn <riazkahanda@gmail.com>
2021-08-09 12:08:27 -0700

    fil added

---
+++ src/component/BannerText/BannerText.js
@@ -0,0 +1,95 @@
+import React from "react";
+import "./BannerText.css";
+
+const BannerText = () => {
+  return (
+    <div
+      id="carouselExampleFade"
+      class="carousel slide carousel-fade"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <div className="caption animate__animated animate__fadeInLeft">
+                  <h6>Welcome to our Doctor Portal </h6>
+                  <h1>Health is all we are concerned about.</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Voluptatibus consequatur sed deserunt deleniti excepturi
+                    nobis ducimus ut aperiam, soluta culpa odio amet ? <br /> Id
+                    unde recusandae consectetur.
+                  </p>
+                  <button>Get Appointment </button>
+                </div>
+              </div>
+              <div className="col-lg-4"></div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <div className="caption animate__animated animate__fadeInLeft animate__delay-1s">
+                  <h6>Welcome to our Doctor Portal </h6>
+                  <h1>Best Care & Better Doctor</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Voluptatibus consequatur sed deserunt deleniti excepturi
+                    nobis ducimus ut aperiam, soluta culpa odio amet ? <br /> Id
+                    unde recusandae consectetur.
+                  </p>
+                  <button>Get Appointment </button>
+                </div>
+              </div>
+              <div className="col-lg-4"></div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <div className="caption animate__animated animate__fadeInLeft animate__delay-1s">
+                  <h6>Welcome to our Doctor Portal </h6>
+                  <h1>We Always Care Your Health</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Voluptatibus consequatur sed deserunt deleniti excepturi
+                    nobis ducimus ut aperiam, soluta culpa odio amet ? <br /> Id
+                    unde recusandae consectetur.
+                  </p>
+                  <button>Get Appointment </button>
+                </div>
+              </div>
+              <div className="col-lg-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleFade"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleFade"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default BannerText;
